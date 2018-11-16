0d499cafc68abd7e754d677662e4ec13adcc730e
Axel Barragán <axelmontes92@gmail.com>
2018-11-16 11:36:06 -0600

    Primer commit
    Se agregaron archivos iniciales y avances primordiales

---
+++ scripts.js
@@ -0,0 +1,46 @@
+/*main.js*/
+$(document).ready(function() {
+
+  $('.owl-carousel').owlCarousel({
+  	margin: 5,
+    loop: true,
+    items: 2,
+    rtl: true,
+    stagePadding: 40,
+  });
+
+  var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
+  	var stickyHeight = sticky.outerHeight();
+  	var stickyTop = stickyWrapper.offset().top;
+  	if (scrollElement.scrollTop() >= stickyTop) {
+  		stickyWrapper.height(stickyHeight);
+  		sticky.addClass("is-sticky");
+  	} else {
+  		sticky.removeClass("is-sticky");
+  		stickyWrapper.height('auto');
+    }
+  };
+
+  $('[data-toggle="sticky-onscroll"]').each(function () {
+  	var sticky = $(this);
+  	var stickyWrapper = $('<div>').addClass('sticky-wrapper');
+  	sticky.before(stickyWrapper);
+  	sticky.addClass('sticky');
+  	$(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
+  		stickyToggle(sticky, stickyWrapper, $(this));
+  	});
+  	stickyToggle(sticky, stickyWrapper, $(window));
+  });
+
+  var scroll = new SmoothScroll('a[data-scroll]', {
+  	speed: 500,
+  	speedAsDuration: true,
+  	offset: 100
+  });
+
+  /*Cerrar el menú después de dar algún "tap" (sólo moviles)*/
+  $('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+});
